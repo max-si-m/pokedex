174cb18aefa0bb3783acba4492f11ed3fae1b99f
Maksym Dzhulii <mak7.dj@gmail.com>
2024-09-11 14:48:04 +0300

    add relp

--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import("fmt")
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type CliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands = map[string]CliCommand{
+  "help": {
+      name:        "help",
+      description: "Displays a help message",
+      callback:    commandHelp,
+  },
+  "exit": {
+      name:        "exit",
+      description: "Exit the Pokedex",
+      callback:    commandExit,
+  },
+}
 
 func main(){
-  fmt.Println("Hello, World!")
+  for {
+    fmt.Printf("Pokedex > ")
+    scanner := bufio.NewScanner(os.Stdin)
+    scanner.Scan()
+
+    command := scanner.Text()
+    if c, ok := commands[command]; ok {
+      c.callback()
+    } else {
+      fmt.Println("Command not found")
+    }
+  }
+}
+
+func commandExit() error {
+	fmt.Println("Exiting program ....")
+	os.Exit(1)
+  return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!\nUsage:")
+
+  fmt.Println("help: Displays a help message")
+	fmt.Println("exit: Exit the Pokedex")
+  return nil
 }
